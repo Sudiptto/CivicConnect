0694fe4964aaab605025889bf6ac164b6d9040e6
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-01-20 21:28:17 -0500

    For 1/21 -> Make sure to finish the validation

--- analysis.py
+++ analysis.py
@@ -3,39 +3,31 @@ from allZipcode import *
 import time
 # Strategy
 """
-Collect all zipcode data, put it in a seperate list and sort it
-After sorting it we can use binary search to verify the zipcode info (log(n) complexity )
+Collect all zipcode data with state data
+Put this data in a hashmap -> look ups will be O(1) complexity and will be used to verify that the data exists and that the state matches the zipcode provided for added credibility 
 """
-# As of 1/20/2024 -> Data has been extracted and all the zipcode data is sorted least to greatest in the allZipcode.py folder -> Please refer to that for any zipcode checking 
-#data = pd.read_csv('geo-data.csv')
-#print(data['zipcode'])
-#zipcode = list(data['zipcode'])
-#print(zipcode)
+# Data turned into a hashmap 
+"""zipcode_Data = {}
 
-# BINARY SEARCH FOR THE ZIPCODE
-#zipcode = zipcode
-#start_time = time.time()
-def binaryZipSearch(zipcode, target):
-    l = 0
-    u = len(zipcode) - l
-    while l <= u:
-        medium = (l + u) // 2
-        if zipcode[medium] == target:
-            return 'FOUND'
-        else:
-            if zipcode[medium] < target:
-                l = medium + 1
-            else:
-                u = medium - 1
-    return 'None'
+df = pd.read_csv('geo-data.csv')
 
-# WORKS 
-print(binaryZipSearch(zipcode, 99950))
-print(binaryZipSearch(zipcode, 11100)) 
+for index, row in df.iterrows():
+    zipcode = str(row['zipcode']).strip()
 
-# Around 1 ms
-"""end_time = time.time()
-execution_time = (end_time - start_time) * 1000
-# 1.001 -> Milliseconds 
-print(f"Execution Time: {execution_time} millisecondss")"""
+    # Check if the zipcode is numeric
+    if zipcode.isnumeric():
+        state = str(row['state_abbr']).strip()
+        zipcode_Data[int(zipcode)] = state
 
+print(zipcode_Data)"""
+
+# analyze the hashmap -> Note for 1/20 -> Further analyze the hashmap and check if the state value matches 
+
+def searchFind(zip, target):
+    if target in zip:
+        return 'Found'
+    else:
+        return 'Not Found'
+    
+print(searchFind(zipcodeData, 11218))
+print(searchFind(zipcodeData, 11200))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, flash
 from passwords import *
+from allZipcode import * 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret
 
