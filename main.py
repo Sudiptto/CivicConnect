85eff24d19e3453deb6b79dfe355618fbd46bce0
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-02-02 21:21:19 -0500

    added verification info -> working on sessions in order to get it working and more specified

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 from passwords import *
 from allZipcode import * 
 from analysis import * 
+from prompts import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret
 
@@ -47,8 +48,8 @@ def home():
 @app.route('/causes',  methods=['GET', 'POST'])
 def causes():
     if not session.get('valid_zip_state'):
-
-        return render_template('error.html')
+        errorMessage = 'Incomplete'
+        return render_template('error.html', errorMessage=errorMessage)
 
     first_name = session.get('first_name')
     zipCode = session.get('ZipCode') #  integer
@@ -60,8 +61,24 @@ def causes():
 
     # NOTE WORKS -> REDIRECT TO AN EMAIL route (final route )
     if request.method == 'POST':
+        
         cause = request.form['cause']
-        print(cause)
+        causeDescription = ""
+        # fill out the prompts -> check the prompts.py file 
+        if cause == 'environmental-protection':
+            causeDescription = environmental_protection
+
+        if cause == 'free-palestine':
+            causeDescription = free_palestine
+
+        if cause == 'racial-redlining':
+            causeDescription = racial_redlining
+
+        if cause == 'poverty':
+            causeDescription = poverty
+        # SESSION FOR EMAIL ROUTE 
+        session['validSubmit'] = True
+        return redirect(url_for('email', emailList = emailList))
 
     return render_template('causes.html', firstName = first_name, nameList = nameList, emailList = emailList)
 
@@ -69,15 +86,21 @@ def causes():
 @app.route('/email')
 def email():
     if not session.get('valid_zip_state'):
-        return render_template('error.html')
+        errorMessage = 'Incomplete'
+        return render_template('error.html', errorMessage = errorMessage)
     
+    if not session.get('validSubmit'):
+        errorMessage = 'NoSubmit'
+        return render_template('error.html', errorMessage = errorMessage)  
+
     return render_template('email.html')
 
 # THIS PAGE -> Users can add / update their queries
 @app.route('/queries')
 def queries():
     if not session.get('valid_zip_state'):
-        return render_template('error.html')
+        errorMessage = 'Incomplete'
+        return render_template('error.html', errorMessage=errorMessage)
     
     first_name = session.get('first_name')
     zipCode = session.get('ZipCode') #  integer
---
+++ prompts.py
@@ -0,0 +1,8 @@
+# PURPOSE OF THIS PYTHON FILE
+
+# this file contains all the prompts for each topic 
+
+environmental_protection = "Filler text: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+free_palestine = "Filler text: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+racial_redlining = "Filler text: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+poverty = "Filler text: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
