5ec150abc353fffe01246bd5c21d11a216fbfd85
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-09-16 11:34:15 -0400

    new change

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from passwords import secret
+from flask_mail import Mail, Message
+
+
 
 # Initialize Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+# for mailing
+mail = Mail(app)
 
 token_data = {}
 reasoning = ""
--- emailsend.py
+++ emailsend.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, flash, redirect, url_for, session
-from flask_mail import Mail, Message
 from app import *
 from passwords import *
 import secrets
@@ -10,32 +9,11 @@ from addData import *
 # Create a Blueprint instance
 email_bp = Blueprint('email_bp', __name__)
 
-# Initialize Flask-Mail
-mail = Mail(app)
-
 def send_verification_email(email, token, reasoning):
-    # to send the verification email use a different email address (for sending)
-    app.config["MAIL_USERNAME"] = verifyCivicEmail
-    app.config["MAIL_PASSWORD"] = verifyCivicAppPass
 
-    # create new mail object
-    verify_mail = Mail(app)
 
-    verification_data = token_data.get(token, {})
 
-    if reasoning == 'queries':
-
-        optionChosen = verification_data.get('selectOption')
-        promptCritique = verification_data.get('promptCritique')
-        subject = 'Verify Your Email for Query'
-        verification_link = url_for('email_bp.verify_email', token=token, reasoning = reasoning, _external=True)
-
-        body = f'Click the following link to verify your email: {verification_link} <br> <b>Here is your inputs:</b> <br> <p>Option chosen: {optionChosen} <br> {promptCritique}  <br> Also please note once you verify please stay on the page for atleast a few seconds so the email can process and be sent!</p>'
-
-        msg = Message(subject, recipients=[email], html=body)
-        verify_mail.send(msg)
-
-    elif reasoning == "sendOnBehalf":
+    if reasoning == "sendOnBehalf":
         # works -> send a verification email out to the user
         #print("Testing again for sendOnBehalf ", verification_data)
         verification_link = url_for('email_bp.verify_email', token=token, reasoning = reasoning, _external=True)
@@ -79,14 +57,10 @@ def send_verification_email(email, token, reasoning):
                 </div>
             </div>
         '''
-
-
         subject = 'Verify Your Email!'
         msg = Message(subject, recipients=[email], html=body)
-        verify_mail.send(msg)
+        mail.send(msg)
 
-        app.config["MAIL_USERNAME"] = emailName
-        app.config["MAIL_PASSWORD"] = emailPassword
 
 
 # VERIFY EMAIL
@@ -95,21 +69,12 @@ def generate_token_and_send_email():
     if request.method == 'POST':
         # Access form data
         email = request.form['email']
-        selectOption = request.form['emailReason']
-        promptCritique = request.form['critique']
         reasoning = request.form['reasoning']
         # Generate a unique token
         verification_token = secrets.token_hex(16)
 
-        if reasoning == 'queries':
-        # Store the token and associated data in the dictionary
-            token_data[verification_token] = {
-                'email': email,
-                'selectOption': selectOption,
-                'promptCritique': promptCritique
-            }
 
-        elif reasoning == "sendOnBehalf":
+        if reasoning == "sendOnBehalf":
             print("Send on behalf activated ")
 
         # Redirect to the route responsible for sending the email
@@ -119,11 +84,7 @@ def generate_token_and_send_email():
 # send the email to the user
 @email_bp.route('/send_verification_email/<email>/<token>/<reasoning>', methods=['GET'])
 def send_verification_email_route(email, token, reasoning):
-    if reasoning == 'queries':
-        send_verification_email(email, token, reasoning=reasoning)
-        flash('Verification link sent to your email. Reasoning: ' + reasoning, category='info')
-
-    elif reasoning == 'sendOnBehalf':
+    if reasoning == 'sendOnBehalf':
         """repEmails = verification_data.get('repEmails')
         repNames = verification_data.get('repNames')
         subject = verification_data.get('subject')
@@ -150,34 +111,12 @@ def verify_email(token, reasoning):
         data = token_data.pop(token)
         print("All data gotten: ", data)
         # Set a session variable to indicate email verification
-        if reasoning == 'queries':
-            session['email_verified'] = True
-            session['email'] = data['email']
-
-            promptCritique = data['promptCritique']
-            optionChosen = data['selectOption']
-            email = data['email']
-
-            flash('Email verified.', category='success')
-
-
-            # send the prompt email to us -> only after verification
-            subject = f'User Prompt: {optionChosen}'
-            body = f'<div style="background-color:#f2f2f2;padding:20px;"><h2 style="color:#333;">User Prompt Details</h2><p><strong>Email:</strong> {email}</p><p><strong>Option chosen:</strong> {optionChosen}</p><p><strong>Prompt Critique:</strong></p><div style="padding-left:20px;">{promptCritique}</div></div>'
-            msg2 = Message(subject, sender=data['email'], recipients=[emailName], cc = [email], html=body)
-            mail.send(msg2)
-            flash('Email sent successfully! Check inbox for more', category='error')
-
-        elif reasoning == 'sendOnBehalf':
+        if reasoning == 'sendOnBehalf':
             # - > worked
             #print("Testing at this app route worked!")
 
             # change the repEmails to seperate emails (for testing -> not final product )
             # site for disposable emails - > https://temp-mail.org/en/
-            repEmailFake = ["fejime7164@ekposta.com", "test@gmail.com"]
-            repNames = ["Testing1", "Testing2"]
-            data['repEmails'] = repEmailFake
-            data["repNames"] = repNames
 
             print(data)
 
@@ -188,15 +127,8 @@ def verify_email(token, reasoning):
             prompt = data['prompt']
             repEmails = data['repEmails']
             repNames = data['repNames']
-
-            # send through the sendcivic gmail (not verifycivic gmail)
-            app.config["MAIL_USERNAME"] = sendEmail
-            app.config["MAIL_PASSWORD"] = sendPassword
             
-            # create new mail object
-            send_mail = Mail(app)
-            
-
+        
             # send the email to the reps -> send one email and send it to all of the rep email addresses
 
             body = f'''
@@ -231,7 +163,7 @@ def verify_email(token, reasoning):
 
             send_msg = Message(subject, sender=userEmail, recipients=repEmails, cc=[userEmail], html=body)
 
-            send_mail.send(send_msg)
+            mail.send(send_msg)
 
             # get the zipcode and the subject and send the data to the analytics file
             zipCode = int(prompt[-5:])
